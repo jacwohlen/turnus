1b0e91d9ae45aa26073692d517db5ae4509ba865
Andreas Schmid <andreas.schmid.as3@roche.com>
2021-09-13 18:40:19 +0200

    Overwrite start page and add turnus logo

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,12 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">turnus</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation2
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <v-container fill-height fluid>
+    <v-row align="center" justify="center">
+      <v-col align="center">
+        <Logo class="rotating" />
+        <h1 class="text-h1">turnus</h1>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -32,34 +16,12 @@ export default Vue.extend({})
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+.rotating {
+  animation: rotateZ 2s;
 }
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+@keyframes rotateZ {
+  to {
+    transform: rotateZ(-360deg);
+  }
 }
 </style>
