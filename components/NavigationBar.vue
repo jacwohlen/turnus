76bad8496c694855c6b280a69fbe645e19913000
Andreas Schmid <andreas.schmid.as3@roche.com>
2021-09-14 00:16:17 +0200

    Turn navbar buttons into tabs and add avatar

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -3,9 +3,15 @@
     <img src="~/assets/turnus-logo.svg" height="70%" alt="turnus logo">
     <h1 class="text-h5 ml-3 mr-3">turnus</h1>
 
-    <v-btn v-for="(l, idx) in links" :key="idx" :to="l.link">
-      {{ l.name }}
-    </v-btn>
+    <v-tabs centered class="ml-n9" color="grey darken-1">
+      <v-tab v-for="(l, idx) in links" :key="idx" :to="l.link">
+        {{ l.name }}
+      </v-tab>
+    </v-tabs>
+
+    <v-avatar>
+      <v-icon>mdi-account-circle</v-icon>
+    </v-avatar>
   </v-app-bar>
 </template>
 
