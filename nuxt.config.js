6c9c1a4b0ae39dbe6008ca29d1d429b85a4196b3
Andreas Schmid <andreas.schmid.as3@roche.com>
2021-09-14 00:08:58 +0200

    Add explicit colors themes for vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,7 +29,24 @@ export default {
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
-    '@nuxtjs/vuetify',
+    [
+      '@nuxtjs/vuetify',
+      {
+        theme: {
+          themes: {
+            light: {
+              primary: '#ff0000',
+              secondary: '#424242',
+              accent: '#82B1FF',
+              error: '#FF5252',
+              info: '#2196F3',
+              success: '#4CAF50',
+              warning: '#FFC107',
+            },
+          },
+        },
+      },
+    ],
     [
       '@nuxtjs/firebase',
       {
