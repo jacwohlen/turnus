534118ebb79cd63fb7c75ebf298b8386f10e615f
Andreas Schmid <andreas.schmid.as3@roche.com>
2021-10-07 18:39:37 +0200

    BracketGenerator: Add tests for roundrobin generator

---
+++ store/pools.generator.test.ts
@@ -0,0 +1,27 @@
+import { BracketGenerator } from './pools'
+
+describe('pools generator', () => {
+  test('test round robin generator', () => {
+    expect(BracketGenerator.roundrobin([], 'pool1')).toEqual([])
+    expect(BracketGenerator.roundrobin(['a', 'b', 'c'], 'pool1')).toEqual([
+      {
+        n: 0,
+        fighter1Id: 'a',
+        fighter2Id: 'b',
+        poolId: 'pool1',
+      },
+      {
+        n: 1,
+        fighter1Id: 'a',
+        fighter2Id: 'c',
+        poolId: 'pool1',
+      },
+      {
+        n: 2,
+        fighter1Id: 'b',
+        fighter2Id: 'c',
+        poolId: 'pool1',
+      },
+    ])
+  })
+})
