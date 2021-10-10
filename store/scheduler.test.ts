fa98cd05f67ae24edfb7ecc3f8a0c1b7a17223c6
Andreas Schmid <andreas.schmid.as3@roche.com>
2021-10-10 15:23:46 +0200

    Fix tests

--- store/scheduler.test.ts
+++ store/scheduler.test.ts
@@ -19,6 +19,7 @@ describe('store/scheduler', () => {
         fighter1Id: 'F1',
         fighter2Id: 'F2',
         poolId: 'Pool1',
+        tatamiScheduled: 'tatami1',
       },
       {
         id: 'Pool1_1',
@@ -26,6 +27,7 @@ describe('store/scheduler', () => {
         fighter1Id: 'F1',
         fighter2Id: 'F3',
         poolId: 'Pool1',
+        tatamiScheduled: 'tatami1',
       },
       {
         id: 'Pool1_2',
@@ -33,6 +35,7 @@ describe('store/scheduler', () => {
         fighter1Id: 'F1',
         fighter2Id: 'F4',
         poolId: 'Pool1',
+        tatamiScheduled: 'tatami1',
       },
       {
         id: 'Pool1_3',
@@ -40,6 +43,7 @@ describe('store/scheduler', () => {
         fighter1Id: 'F2',
         fighter2Id: 'F3',
         poolId: 'Pool1',
+        tatamiScheduled: 'tatami1',
       },
       {
         id: 'Pool1_4',
@@ -47,6 +51,7 @@ describe('store/scheduler', () => {
         fighter1Id: 'F2',
         fighter2Id: 'F4',
         poolId: 'Pool1',
+        tatamiScheduled: 'tatami1',
       },
       {
         id: 'Pool1_5',
@@ -54,24 +59,38 @@ describe('store/scheduler', () => {
         fighter1Id: 'F3',
         fighter2Id: 'F4',
         poolId: 'Pool1',
+        tatamiScheduled: 'tatami1',
       },
     ]
-    expect(schedulerStore.getCurrentMatch).toBeNull()
-    expect(schedulerStore.getNextMatch).toBeNull()
-    await schedulerStore.next()
+    expect(schedulerStore.getCurrentMatch('tatami1')).toBeNull()
+    expect(schedulerStore.getNextMatch('tatami1')).toBeNull()
+    await schedulerStore.next('tatami1')
     await schedulerStore.addMatches(matches)
-    expect(schedulerStore.getStats).toEqual({ upcoming: 6, done: 0 })
+    await schedulerStore.scheduleMatches({ tatamiId: 'tatami1', matches })
+    expect(schedulerStore.getStats('tatami1')).toEqual({
+      upcoming: 6,
+      done: 0,
+      total: 6,
+    })
 
-    expect(schedulerStore.getCurrentMatch).toEqual(matches[0])
-    expect(schedulerStore.getNextMatch).toEqual(matches[1])
+    expect(schedulerStore.getCurrentMatch('tatami1')).toEqual(matches[0])
+    expect(schedulerStore.getNextMatch('tatami1')).toEqual(matches[1])
 
-    await schedulerStore.next()
-    expect(schedulerStore.getCurrentMatch).toStrictEqual(matches[1])
-    expect(schedulerStore.getStats).toEqual({ upcoming: 5, done: 1 })
+    await schedulerStore.next('tatami1')
+    expect(schedulerStore.getCurrentMatch('tatami1')).toStrictEqual(matches[1])
+    expect(schedulerStore.getStats('tatami1')).toEqual({
+      upcoming: 5,
+      done: 1,
+      total: 6,
+    })
 
-    await schedulerStore.previous()
-    expect(schedulerStore.getCurrentMatch).toStrictEqual(matches[0])
-    expect(schedulerStore.getStats).toEqual({ upcoming: 6, done: 0 })
+    await schedulerStore.previous('tatami1')
+    expect(schedulerStore.getCurrentMatch('tatami1')).toStrictEqual(matches[0])
+    expect(schedulerStore.getStats('tatami1')).toEqual({
+      upcoming: 6,
+      done: 0,
+      total: 6,
+    })
   })
 
   test('test cycling through scheduling', async () => {
@@ -82,6 +101,7 @@ describe('store/scheduler', () => {
         fighter2Id: 'F2',
         poolId: 'Pool1',
         id: 'Pool1_0',
+        tatamiScheduled: 'tatami1',
       },
       {
         n: 1,
@@ -89,6 +109,7 @@ describe('store/scheduler', () => {
         fighter2Id: 'F3',
         poolId: 'Pool1',
         id: 'Pool1_1',
+        tatamiScheduled: 'tatami1',
       },
       {
         n: 2,
@@ -96,22 +117,24 @@ describe('store/scheduler', () => {
         fighter2Id: 'F4',
         poolId: 'Pool1',
         id: 'Pool1_2',
+        tatamiScheduled: 'tatami1',
       },
     ]
     await schedulerStore.addMatches(matches)
+    await schedulerStore.scheduleMatches({ tatamiId: 'tatami1', matches })
 
-    await schedulerStore.previous()
-    expect(schedulerStore.getCurrentMatch).toStrictEqual(matches[0])
-    await schedulerStore.next()
-    expect(schedulerStore.getCurrentMatch).toStrictEqual(matches[1])
-    await schedulerStore.next()
-    expect(schedulerStore.getCurrentMatch).toStrictEqual(matches[2])
-    await schedulerStore.next()
-    expect(schedulerStore.getCurrentMatch).toStrictEqual(matches[2])
-    await schedulerStore.previous()
-    expect(schedulerStore.getCurrentMatch).toStrictEqual(matches[1])
-    await schedulerStore.previous()
-    expect(schedulerStore.getCurrentMatch).toStrictEqual(matches[0])
-    await schedulerStore.previous()
+    await schedulerStore.previous('tatami1')
+    expect(schedulerStore.getCurrentMatch('tatami1')).toStrictEqual(matches[0])
+    await schedulerStore.next('tatami1')
+    expect(schedulerStore.getCurrentMatch('tatami1')).toStrictEqual(matches[1])
+    await schedulerStore.next('tatami1')
+    expect(schedulerStore.getCurrentMatch('tatami1')).toStrictEqual(matches[2])
+    await schedulerStore.next('tatami1')
+    expect(schedulerStore.getCurrentMatch('tatami1')).toStrictEqual(matches[2])
+    await schedulerStore.previous('tatami1')
+    expect(schedulerStore.getCurrentMatch('tatami1')).toStrictEqual(matches[1])
+    await schedulerStore.previous('tatami1')
+    expect(schedulerStore.getCurrentMatch('tatami1')).toStrictEqual(matches[0])
+    await schedulerStore.previous('tatami1')
   })
 })
