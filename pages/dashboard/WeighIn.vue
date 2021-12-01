022f3c5d604fa86f8f3ef8f94959fc7a1f08b388
Pascal Lüscher <pascal.luescher@asinfotrack.ch>
2021-12-01 14:34:22 +0100

    class component WeighIn.vue

--- pages/dashboard/WeighIn.vue
+++ pages/dashboard/WeighIn.vue
@@ -4,24 +4,24 @@
     <v-card>
       <v-card-title>
         Competitors
-        <v-spacer></v-spacer>
+        <v-spacer/>
         <v-text-field
           v-model="search"
           append-icon="mdi-magnify"
+          hide-details
           label="Search"
           single-line
-          hide-details
-        ></v-text-field>
+        />
       </v-card-title>
       <v-data-table
         :headers="headers"
         :items="items"
         :search="search"
-        hide-default-footer
         disable-pagination
+        hide-default-footer
       >
         <template v-slot:item.action="{ item }">
-          <CheckinForm :prefilled="item" />
+          <CheckinForm :prefilled="item"/>
         </template>
       </v-data-table>
     </v-card>
@@ -29,39 +29,42 @@
 </template>
 
 <script lang="ts">
-import { competitorsStore, categoriesStore, poolsStore } from '~/store'
+import {competitorsStore, categoriesStore, poolsStore} from '~/store'
+import {Component, Vue} from 'vue-property-decorator';
+import {Competitor} from '~/types/models';
+import {DataTableHeader} from 'vuetify';
 
-export default {
+@Component({
   layout: 'DashboardLayout',
-  async fetch() {
-    await competitorsStore.init()
-    await competitorsStore.init()
-    await categoriesStore.init()
-    await poolsStore.init()
-  },
-  async mounted() {
-    await competitorsStore.init()
-    await categoriesStore.init()
-    await poolsStore.init()
-  },
-  data: () => ({
-    search: '',
-    headers: [
-      { text: 'Firstname', value: 'firstname' },
-      { text: 'Lastname', value: 'lastname' },
-      { text: 'Sex', value: 'sex' },
-      { text: 'Birthday', value: 'birthyear' },
-      { text: 'Club', value: 'club' },
-      { text: 'Given Weight', value: 'weight' },
-      { text: 'Measured Weight', value: 'weightMeasured' },
-      { text: 'Action', value: 'action' },
-    ],
-    selectedCategory: null,
-  }),
-  computed: {
-    items() {
-      return competitorsStore.list
-    },
-  },
+})
+export default class WeighIn extends Vue {
+  private search: string = '';
+  private headers: DataTableHeader<Competitor>[] = [
+    {text: 'Firstname', value: 'firstname'},
+    {text: 'Lastname', value: 'lastname'},
+    {text: 'Sex', value: 'sex'},
+    {text: 'Birthday', value: 'birthyear'},
+    {text: 'Club', value: 'club'},
+    {text: 'Given Weight', value: 'weight'},
+    {text: 'Measured Weight', value: 'weightMeasured'},
+    {text: 'Action', value: 'action'}
+  ];
+
+  private get items(): Competitor[] {
+    return competitorsStore.list;
+  }
+
+  public async fetch() {
+    await competitorsStore.init();
+    await competitorsStore.init();
+    await categoriesStore.init();
+    await poolsStore.init();
+  }
+
+  public async mounted() {
+    await competitorsStore.init();
+    await categoriesStore.init();
+    await poolsStore.init();
+  }
 }
 </script>
