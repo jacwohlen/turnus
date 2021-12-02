fa4c3fe8be1dfd5074c065545fe45dcc27500447
Isitar <luescherpascal@gmail.com>
2021-12-02 21:48:12 +0100

    cleanup Competitors.vue as class-component

--- pages/dashboard/Competitors.vue
+++ pages/dashboard/Competitors.vue
@@ -1,53 +1,58 @@
 <template>
   <div>
-    <h1>Competitors</h1>
+    <PageTitle title="Competitors" />
     <CompetitorForm />
     <v-simple-table>
-      <template v-slot:default>
-        <thead>
-          <tr>
-            <th class="text-left">First Name</th>
-            <th class="text-left">Last Name</th>
-            <th class="text-left">Sex</th>
-            <th class="text-left">Birthday</th>
-            <th class="text-left">Club</th>
-            <th class="text-left">Weight</th>
-            <th class="text-left">Pools</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(item, idx) in items" :key="idx">
-            <td>{{ item.firstname }}</td>
-            <td>{{ item.lastname }}</td>
-            <td>{{ item.sex }}</td>
-            <td>{{ item.birthyear }}</td>
-            <td>{{ item.club }}</td>
-            <td>{{ item.weight }}</td>
-            <td>
-              <v-chip v-for="p in item.pools" :key="p.id">{{ p.name }}</v-chip>
-            </td>
-          </tr>
-        </tbody>
-      </template>
+      <thead>
+        <tr>
+          <th class="text-left">First Name</th>
+          <th class="text-left">Last Name</th>
+          <th class="text-left">Sex</th>
+          <th class="text-left">Birthday</th>
+          <th class="text-left">Club</th>
+          <th class="text-left">Weight</th>
+          <th class="text-left">Pools</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, idx) in items" :key="idx">
+          <td>{{ item.firstname }}</td>
+          <td>{{ item.lastname }}</td>
+          <td>{{ item.sex }}</td>
+          <td>{{ item.birthyear }}</td>
+          <td>{{ item.club }}</td>
+          <td>{{ item.weight }}</td>
+          <td>
+            <v-chip v-for="p in item.pools" :key="p.id">{{ p.name }}</v-chip>
+          </td>
+        </tr>
+      </tbody>
     </v-simple-table>
   </div>
 </template>
 
 <script lang="ts">
-import { competitorsStore } from '~/store'
+import { Component, Vue } from 'vue-property-decorator';
+import { competitorsStore } from '~/store';
+import { Competitor } from '~/types/models';
+import PageTitle from '~/components/common/PageTitle.vue';
+import CompetitorForm from '~/components/CompetitorForm.vue';
 
-export default {
+@Component({
+  components: { CompetitorForm, PageTitle },
   layout: 'DashboardLayout',
-  fetch() {
-    competitorsStore.init()
-  },
-  computed: {
-    items() {
-      return competitorsStore.list
-    },
-  },
-  mounted() {
-    competitorsStore.init()
-  },
+})
+export default class Competitors extends Vue {
+  private get items(): Competitor[] {
+    return competitorsStore.list;
+  }
+
+  public async fetch() {
+    await competitorsStore.init();
+  }
+
+  public async mounted() {
+    await competitorsStore.init();
+  }
 }
 </script>
