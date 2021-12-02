dd78036f179e46738170110413d550ea33120766
Isitar <luescherpascal@gmail.com>
2021-12-02 22:07:35 +0100

    cleanup DashboardLayout.vue fixed mobile overflow

--- layouts/DashboardLayout.vue
+++ layouts/DashboardLayout.vue
@@ -9,10 +9,25 @@
     </v-main>
   </v-app>
 </template>
-<style scoped>
+
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import NavigationBar from '~/components/NavigationBar.vue';
+import Alert from '~/components/Alert.vue';
+
+@Component({
+  components: { Alert, NavigationBar },
+})
+export default class DashboardLayout extends Vue {
+
+}
+</script>
+
+<style lang="scss" scoped>
 .content {
   max-width: 1200px;
-  min-width: 1200px;
+  min-width: calc(min(100%, 1200px));
   margin: 0 auto;
   background-color: white;
 }
