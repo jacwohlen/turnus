10ddca332507aafdeb2a952699b583ceffafc850
Isitar <luescherpascal@gmail.com>
2021-12-12 13:42:16 +0100

    refactored alert

--- components/Alert.vue
+++ components/Alert.vue
@@ -1,46 +1,51 @@
 <template>
-  <div>
+  <div
+    class="wrapper"
+    :style="{
+      top: top + 'px',
+    }"
+  >
     <v-alert
-      class="text-center"
+      class="mt-3"
       :value="!!alert['message']"
       :type="alert.type"
-      absolute
       dismissible
       border="left"
       elevation="5"
       @input="closeAlert"
-      >{{ alert['message'] }}</v-alert
     >
+      {{ alert['message'] }}
+    </v-alert>
   </div>
 </template>
 
 <script lang="ts">
-import { alertStore } from '~/store'
+import { Component, Vue } from 'vue-property-decorator';
+import { alertStore } from '~/store';
+import { AlertMessage } from '~/store/alert';
 
-export default {
-  computed: {
-    alert() {
-      if (alertStore === undefined) {
-        console.log('Error: alertStore must not be undefined')
-      }
-      return alertStore.alert
-    },
-  },
+@Component
+export default class Alert extends Vue {
+  private get alert(): AlertMessage {
+    return alertStore.alert;
+  }
 
-  methods: {
-    closeAlert() {
-      alertStore.setError({ msg: '' })
-    },
-  },
+  private closeAlert(): void {
+    alertStore.setError({ msg: '' });
+  }
+
+  public get top(): number {
+    return this.$vuetify.application.top;
+  }
 }
 </script>
-<style scoped>
-.v-alert {
+
+<style lang="scss" scoped>
+.wrapper {
   position: fixed;
-  left: 50%;
-  top: 120px;
-  transform: translate(-50%, -50%);
-  margin: 0 auto;
+  display: inline-grid;
+  justify-content: center;
+  width: 100%;
   z-index: 9999;
 }
 </style>
